922eca117c1effa3d3257f5c2efc78de37368970
Yaroslav Kuchmenko <yaroslav.kuchmenko@ukma.edu.ua>
2020-10-01 00:04:50 +0300

    main misc refactor

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,18 @@ int main(int argc, char *argv[])
     setLooks(mainWindow, a, mwf);
 
     mainWindow.show();
-    return a.exec();
+    return QApplication::exec();
 }
 
 
 void setLooks(QMainWindow& m, QApplication& a, Ui::MainWindowForm& mwf){
     m.setWindowFlags(Qt::MSWindowsFixedSizeDialogHint);
-    a.setStyle(QStyleFactory::create("Fusion"));
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
 
-    QPixmap bkgnd(":/green_background.png");
-    bkgnd = bkgnd.scaled(m.size(), Qt::IgnoreAspectRatio);
+    QPixmap background(":/green_background.png");
+    background = background.scaled(m.size(), Qt::IgnoreAspectRatio);
     QPalette palette;
-    palette.setBrush(QPalette::Background, bkgnd);
+    palette.setBrush(QPalette::Background, background);
     m.setPalette(palette);
 
     mwf.display->setStyleSheet(" border-image: url(:/blue_screen.png) 0 0 0 0 stretch stretch; border-width: 0px;");
