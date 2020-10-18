baf9b712b1790f6195522711819a3b813647af2f
Tori <yankiniigor@gmail.com>
2020-10-18 18:50:55 +0300

    add constructor to SessionManager header

--- src/model/SessionManager.h
+++ src/model/SessionManager.h
@@ -4,6 +4,8 @@ private:
 	static unsigned int maxIdleTime_;
 	time_t lastActionTime_;
 public:
+	SessionManager(time_t);
+
 	bool checkExpiration();
 	//update to current date and time
 	void updateLastActionTime();
