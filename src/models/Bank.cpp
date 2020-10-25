97d1648cf92e06889c3f9b96544866628b087e11
darkllen <yankiniigor@gmail.com>
2020-10-25 03:08:38 +0300

    add right connection setup

--- src/models/Bank.cpp
+++ src/models/Bank.cpp
@@ -6,6 +6,10 @@
 
 #include "accounts/Account.h"
 #include <mysqlx/xdevapi.h>
+#include <iostream>
+#include <string>
+
+using namespace mysqlx;
 
 Bank::Bank(BANK_NUMBER_T id, std::string address, std::string name) :
         id_(id), address_(std::move(address)),
@@ -13,7 +17,13 @@ Bank::Bank(BANK_NUMBER_T id, std::string address, std::string name) :
 
 PIN_T Bank::getCardPIN(const CARD_NUMBER_T & cardNumberT) {
     //TODO: Requires implementation
-    mysqlx::Session sess("localhost", 3306, "root", "0987654321i");
+    try {
+        const char *url = ("mysqlx://root:qwerty@91.196.194.253:33060");
+        Session mySession(url);
+    } catch (std::exception e) {
+        std::cout << e.what()<<std::endl;
+    }
+
 
     //mysqlx::Schema db= sess.getSchema("moop");
 
