5e8d5c2b89d4307651d729e22327df7228b24f90
darkllen <yankiniigor@gmail.com>
2020-10-25 17:03:50 +0200

    fix includes

--- src/models/Bank.cpp
+++ src/models/Bank.cpp
@@ -4,13 +4,9 @@
 
 #include "Bank.h"
 
-#include "accounts/Account.h"
 #include "../exceptions/ATMException.h"
 #include <mysqlx/xdevapi.h>
-#include <iostream>
 #include <string>
-
-using namespace mysqlx;
 
 Bank::Bank(BANK_NUMBER_T id, std::string address, std::string name) :
         id_(id), address_(std::move(address)),
@@ -19,13 +15,13 @@ Bank::Bank(BANK_NUMBER_T id, std::string address, std::string name) :
 PIN_T Bank::getCardPIN(const CARD_NUMBER_T & cardNumberT) {
     try {
         const char *url = ("mysqlx://root:qwerty@91.196.194.253:33060");
-        Session session(url);
-        Schema db = session.getSchema("moop");
-        Table myTable = db.getTable("DebitCard");
-        RowResult myResult = myTable.select("PIN_")
+        mysqlx::Session session(url);
+        mysqlx::Schema db = session.getSchema("moop");
+        mysqlx::Table myTable = db.getTable("DebitCard");
+        mysqlx::RowResult myResult = myTable.select("PIN_")
                 .where("cardNum_ like :cardNum_")
                 .bind("cardNum_",cardNumberT).execute();
-        Row row = myResult.fetchOne();
+        mysqlx::Row row = myResult.fetchOne();
         return row.get(0).get<int>();
     } catch (std::exception& e) {
         throw DBException(e.what());
