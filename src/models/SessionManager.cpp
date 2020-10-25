53d889cd79ae50bbca037bc4ae4c699e7d7d0705
Ana22102000 <45357776+Ana22102000@users.noreply.github.com>
2020-10-25 19:30:07 +0200

    realize checkExpiration

--- src/models/SessionManager.cpp
+++ src/models/SessionManager.cpp
@@ -11,7 +11,7 @@ SessionManager::SessionManager() : lastActionTime_(QDateTime::currentDateTime())
 
 bool SessionManager::checkExpiration() {
     QDateTime current = QDateTime::currentDateTime();
-    QDateTime checkAgainst(current.addSecs(ATMLimits::MAX_SESSION_IDLE_TIME_SEC));
+    QDateTime checkAgainst(lastActionTime_.addSecs(ATMLimits::MAX_SESSION_IDLE_TIME_SEC));
     return current < checkAgainst;
 }
 
