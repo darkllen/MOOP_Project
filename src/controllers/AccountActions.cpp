b820b9129b909bc48924fff6013a1463555bd090
darkllen <yankiniigor@gmail.com>
2020-10-26 10:17:58 +0200

    implement DB methods in other methods according to TODO

--- src/controllers/AccountActions.cpp
+++ src/controllers/AccountActions.cpp
@@ -4,10 +4,30 @@
 
 #include "AccountActions.h"
 
-void AccountActions::makeTransaction(const Account &, const Transaction &) {
+void AccountActions::makeTransaction(const Account & acc, const Transaction & tr) {
     //TODO: Requires implementation
 }
 
 void AccountActions::viewHistory(const Account &) {
     //TODO: Requires implementation
+
+//    try {
+//        const char *url = ("mysqlx://root:qwerty@91.196.194.253:33060");
+//        mysqlx::Session session(url);
+//        mysqlx::Schema db = session.getSchema("moop");
+//        mysqlx::Table myTable = db.getTable("Transactions");
+//        mysqlx::RowResult myResult = myTable.select("id","time_","from_","to_",	"amount_",	"regularity_",	"isWithdrawal_",	"valueChanged_",	"oldValue_",	"newValue_")
+//                .where("IBAN_ like :IBAN_")
+//                .bind("IBAN_",acc.getIBAN_()).execute();
+//
+//
+//        for(size_t i = 0; i<= myResult.count(); ++i){
+//            mysqlx::Row row = myResult.fetchOne();
+//            int id (row[0].get<int>());
+//            std::string time(row[1].get<std::string>());
+//        }
+//        return;
+//    } catch (std::exception& e) {
+//        throw DBException(e.what());
+//    }
 }
--- src/models/atm_hardware/CardReader.cpp
+++ src/models/atm_hardware/CardReader.cpp
@@ -2,6 +2,7 @@
 // Created by anna2 on 20.10.2020.
 //
 #include "CardReader.h"
+#include "../../models/Bank.h"
 #include "../../controllers/PinVerificationService.h"
 #include "../../constants/ATMLimits.h"
 #include "../DebitCard.h"
@@ -21,7 +22,8 @@ void CardReader::evalPIN(const PIN_T pin) {
 void CardReader::setInsertedCardN(const CARD_NUMBER_T n) {
     inserted_card_n_ = n;
     //check if card is blocked
-    DebitCard debitCard = DebitCard(0, QDateTime(),0,0);//TODO get card from db (number = inserted_card_n_)
+    //TODO catch  BDexception
+    DebitCard debitCard = Bank::getCard(inserted_card_n_);
     if(debitCard.getIsBlocked())
         //TODO: write that card is blocked
         returnCard();
@@ -36,7 +38,8 @@ void CardReader::onVerificationFail() {
     if (evalTries == ATMLimits::MAX_FAILED_PIN_EVALS){
         // don't return the card
         //TODO: Requires implementation
-        DebitCard debitCard = DebitCard(0, QDateTime(),0,0);//TODO get card from db (number = inserted_card_n_)
+        //TODO catch  BDexception
+        DebitCard debitCard = Bank::getCard(inserted_card_n_);
         debitCard.setIsBlocked(true);
         reset();
     }
