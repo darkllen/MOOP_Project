73e2fb8c30cdf07668c064184f9a75260dc98d3a
darkllen <yankiniigor@gmail.com>
2020-10-26 10:18:50 +0200

    remove method

--- src/models/Bank.cpp
+++ src/models/Bank.cpp
@@ -15,23 +15,7 @@
 Bank::Bank(BANK_NUMBER_T id, std::string address, std::string name) :
         id_(id), address_(std::move(address)),
         name_(std::move(name)) {}
-
-PIN_T Bank::getCardPIN(const CARD_NUMBER_T & cardNumberT) {
-    try {
-        const char *url = ("mysqlx://root:qwerty@91.196.194.253:33060");
-        mysqlx::Session session(url);
-        mysqlx::Schema db = session.getSchema("moop");
-        mysqlx::Table myTable = db.getTable("DebitCard");
-        mysqlx::RowResult myResult = myTable.select("PIN_")
-                .where("cardNum_ like :cardNum_")
-                .bind("cardNum_",cardNumberT).execute();
-        mysqlx::Row row = myResult.fetchOne();
-        return row.get(0).get<int>();
-    } catch (std::exception& e) {
-        throw DBException(e.what());
-    }
-}
-
+        
 DebitCard Bank::getCard(const CARD_NUMBER_T& cardNumberT) {
     try {
         const char *url = ("mysqlx://root:qwerty@91.196.194.253:33060");
