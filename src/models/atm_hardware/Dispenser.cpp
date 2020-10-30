e92deae6b4b3f24f3562d301fa0db0b968fb5d2d
Ana22102000 <45357776+Ana22102000@users.noreply.github.com>
2020-10-30 18:30:58 +0200

    add exceptions dispenser

--- src/models/atm_hardware/Dispenser.cpp
+++ src/models/atm_hardware/Dispenser.cpp
@@ -5,23 +5,27 @@
 #include "Dispenser.h"
 
 #include "../../constants/ATMLimits.h"
+#include "../../exceptions/ATMException.h"
 
 Dispenser::Dispenser(CASH_AMOUNT_T amount): Hardware(), availableCash_(amount) {
     if (amount >= ATMLimits::MAX_CASH_AMOUNT_IN_DISPENSER) {
-        //TODO: Requires implementation
+        throw HardwareException("More cash then dispenser can handle");
+        //TODO: Show that in ui
     }
 }
 
 void Dispenser::cashIn(CASH_AMOUNT_T amount) {
     if (availableCash_ + amount >= ATMLimits::MAX_CASH_AMOUNT_IN_DISPENSER) {
-        //TODO: Requires implementation
+        throw HardwareException("More cash then dispenser can handle");
+        //TODO: Show that in ui
     }
     availableCash_ += amount;
 }
 
 void Dispenser::cashOut(CASH_AMOUNT_T amount) {
     if (availableCash_ - amount < 0){
-        //TODO: Requires implementation
+        throw HardwareException("Not enough cash left in dispenser");
+        //TODO: Show that in ui
     }
     availableCash_ -= amount;
 }
