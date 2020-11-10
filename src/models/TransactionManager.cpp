37df7d028994924698d398efae2b68c488bbb4ed
darkllen <yankiniigor@gmail.com>
2020-11-10 16:46:04 +0200

    fix memory leak

--- src/models/TransactionManager.cpp
+++ src/models/TransactionManager.cpp
@@ -7,42 +7,41 @@
 #include "accounts/SavingAccount.h"
 #include "../exceptions/ATMException.h"
 
-const OneTimeTransfer &TransactionManager::createTransaction(const QDateTime & date, const Account & to, const Account & from, const CASH_AMOUNT_T & amount) {
+OneTimeTransfer TransactionManager::createTransaction(const QDateTime & date, const Account & to, const Account & from, const CASH_AMOUNT_T & amount) {
     if (const auto* t = dynamic_cast<const CheckingAccount*>(&from)){
-        return *(new OneTimeTransfer(date, to, from, amount));
+        return OneTimeTransfer(date, to, from, amount);
     } else  if (const auto* t = dynamic_cast<const SavingAccount*>(&from)){
         if(t->getMoney() >= t->getLimit() + amount)
-            return *(new OneTimeTransfer(date, to, from, amount));
+            return OneTimeTransfer(date, to, from, amount);
          else
             throw TransactionException("You can't take this amount of money because of card limit");
     } else throw TransactionException("Wrong account type");
 
 }
 
-const RegularTransfer &TransactionManager::createTransaction(const QDateTime & date, const Account & to, const Account & from, const CASH_AMOUNT_T & amount, const int& reg) {
+RegularTransfer TransactionManager::createTransaction(const QDateTime & date, const Account & to, const Account & from, const CASH_AMOUNT_T & amount, const int& reg) {
     if (const auto* t = dynamic_cast<const CheckingAccount*>(&from)){
-        return *(new RegularTransfer(date, to, from, amount, reg));
+        return RegularTransfer(date, to, from, amount, reg);
     } else  if (const auto* t = dynamic_cast<const SavingAccount*>(&from)){
         if(t->getMoney() >= t->getLimit() + amount)
-            return *(new RegularTransfer(date, to, from, amount, reg));
+            return RegularTransfer(date, to, from, amount, reg);
         else
             throw TransactionException("You can't take this amount of money because of card limit");
     } else throw TransactionException("Wrong account type");
-
 }
 
-const CashTransaction &TransactionManager::createTransaction(const QDateTime & date, const Account & from, const ACCOUNT_BALANCE_AMOUNT_T & amount, const bool& isWithdrawal) {
+CashTransaction TransactionManager::createTransaction(const QDateTime & date, const Account & from, const ACCOUNT_BALANCE_AMOUNT_T & amount, const bool& isWithdrawal) {
     if (const auto* t = dynamic_cast<const CheckingAccount*>(&from)){
-        return *(new CashTransaction(date, from, amount, isWithdrawal));
+        return CashTransaction(date, from, amount, isWithdrawal);
     } else  if (const auto* t = dynamic_cast<const SavingAccount*>(&from)){
         if (((isWithdrawal)&&(t->getMoney() >= t->getLimit() + amount))||(!isWithdrawal))
-            return *(new CashTransaction(date, from, amount, isWithdrawal));
+            return CashTransaction(date, from, amount, isWithdrawal);
         else
             throw TransactionException("You can`t take this amount of money because of card limit");
     } else throw TransactionException("Wrong account type");
 
     }
 
-const AccountManaging &TransactionManager::createTransaction(const QDateTime & date,const Account & from, const CARD_NUMBER_T & card_n, const AccountManaging::ValueChanged& valueType, const VAL& oldValue, const VAL& newValue) {
-    return *(new AccountManaging(date, from, card_n, valueType, oldValue, newValue));
+AccountManaging TransactionManager::createTransaction(const QDateTime & date,const Account & from, const CARD_NUMBER_T & card_n, const AccountManaging::ValueChanged& valueType, const VAL& oldValue, const VAL& newValue) {
+    return AccountManaging(date, from, card_n, valueType, oldValue, newValue);
 }
--- src/models/TransactionManager.h
+++ src/models/TransactionManager.h
@@ -24,10 +24,10 @@ public:
     TransactionManager() = default;
     ~TransactionManager() = default;
 
-    static const OneTimeTransfer &createTransaction(const QDateTime &, const Account &, const Account &, const CASH_AMOUNT_T &);
-    static const RegularTransfer &createTransaction(const QDateTime &, const Account &, const Account &, const CASH_AMOUNT_T &, const int&);
-    static const CashTransaction &createTransaction(const QDateTime &, const Account &, const ACCOUNT_BALANCE_AMOUNT_T &, const bool&);
-    static const AccountManaging &createTransaction(const QDateTime &, const Account &,  const CARD_NUMBER_T & , const AccountManaging::ValueChanged &, const VAL&, const VAL&);
+    static OneTimeTransfer createTransaction(const QDateTime &, const Account &, const Account &, const CASH_AMOUNT_T &);
+    static RegularTransfer createTransaction(const QDateTime &, const Account &, const Account &, const CASH_AMOUNT_T &, const int&);
+    static CashTransaction createTransaction(const QDateTime &, const Account &, const ACCOUNT_BALANCE_AMOUNT_T &, const bool&);
+    static AccountManaging createTransaction(const QDateTime &, const Account &,  const CARD_NUMBER_T & , const AccountManaging::ValueChanged &, const VAL&, const VAL&);
 };
 
 #endif //MOOP_ATM_PROJECT_TRANSACTIONMANAGER_H
